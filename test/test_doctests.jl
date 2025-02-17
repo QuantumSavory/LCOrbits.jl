c7f8e647aba0f184aa0faebd10c2d5f27c1a7e36
Stefan Krastanov <stefan@krastanov.org>
2025-02-17 09:38:22 -0500

    fix tests

--- test/test_doctests.jl
+++
@@ -1,9 +0,0 @@
-@testitem "doctests" begin
-
-using Documenter
-using BPGates
-
-DocMeta.setdocmeta!(BPGates, :DocTestSetup, :(using QuantumClifford, BPGates); recursive=true)
-doctest(BPGates)
-
-end
